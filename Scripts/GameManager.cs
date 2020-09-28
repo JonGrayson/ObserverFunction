9381b5187b91275c98f1257ea943ce34bd4113f0
JonGrayson <49501559+JonGrayson@users.noreply.github.com>
2020-09-27 21:45:40 -0700

    Add files via upload

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	bool gameHasEnded = false;
+
+	public float restartDelay = 1f;
+
+	public GameObject completeLevelUI;
+
+    public GameObject player;
+
+	public void CompleteLevel ()
+	{
+		completeLevelUI.SetActive(true);
+	}
+
+	public void EndGame ()
+	{
+		if (gameHasEnded == false)
+		{
+			gameHasEnded = true;
+			Debug.Log("GAME OVER");
+			Invoke("Restart", restartDelay);
+		}
+	}
+
+	void Restart ()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+    //alarm recieved
+    private void OnEnable()
+    {
+        PlayerMovement.onColorChange += ColorChange;
+    }
+
+    void ColorChange()
+    {
+        player.GetComponent<Renderer>().material.SetColor("_Color", Color.cyan);
+    }
+
+}
